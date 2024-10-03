c437bc6fa4f3d81c4e7a90800a9d4641b8c632fa
Stiwii <stiwers1997@gmail.com>
2024-10-03 03:40:19 -0500

    SS: auth

--- src/auth.ts
+++ src/auth.ts
@@ -1,7 +1,12 @@
-
 import NextAuth from "next-auth"
-import Google from "next-auth/providers/google"
 
-export const { handlers, signIn, signOut, auth } = NextAuth({
-    providers: [Google],
+export const { handlers, auth, signIn, signOut } = NextAuth({
+    callbacks: {
+        async signIn({ account, profile }) {
+            if (account.provider === "google") {
+                return profile.email_verified && profile.email.endsWith("@example.com")
+            }
+            return true // Do different verification for other providers that don't have `email_verified`
+        },
+    },
 })
