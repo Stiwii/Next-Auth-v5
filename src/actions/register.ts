d848a96a9bc7d4bb45eb5e2dc5555d923827ca9d
Stiwii <stiwers1997@gmail.com>
2024-10-11 01:42:22 -0500

    SS: middleware and login

--- src/actions/register.ts
+++ src/actions/register.ts
@@ -1,12 +1,33 @@
 "use server";
 
 import * as z from "zod";
+import bcrypt from "bcrypt";
 import { RegisterSchema } from "@/schemas";
+
+import { db } from "@/lib/db";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
     const validateFields = RegisterSchema.safeParse(values);
     if (!validateFields.success) {
         return { error: "Invalid fields" }
     }
-    return { success: "Email sent" };
+    const { email, password, name } = validateFields.data;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const existingUser = await getUserByEmail(email);
+    if (existingUser) {
+        return { error: "Email already in use!" }
+    }
+
+    await db.user.create({
+        data: {
+            email,
+            password: hashedPassword,
+            name,
+        },
+    });
+
+    //TODO: send verification token email
+    return { success: "User created" };
 };
---
+++ src/routes.ts
@@ -0,0 +1,26 @@
+/**
+ * An Array ofroutes that are public and should not be protected by authentication
+ * @type {string[]}
+ */
+export const publicRoutes = ["/"];
+
+/**
+ * An Array of routes that are protected by authentication
+ * these routes will be redirected to the login page if the user is not authenticated
+ * @type {string[]}
+ */
+
+export const authRoutes = ["/auth/login", "/auth/register"];
+
+/**
+ * The prefix for API authentication routes
+ * Routes that start with this prefix are used for authentication purposes
+ * @type {string[]}
+ */
+export const apiAuthPrefix = "/api/auth";
+
+/**
+ * The default redirect path after successful login
+ * @type {string}
+ */
+export const DEFAULT_LOGIN_REDIRECT = "/settings";
